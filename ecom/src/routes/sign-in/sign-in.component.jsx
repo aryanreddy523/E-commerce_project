012e9d280182dba1a1c534fb8bb2ba42e5a2649f
Aryan Reddy <kalluriaryanreddy@Aryans-MacBook-Air.local>
2023-04-09 11:09:57 +0530

    added sign-in component and firebase

---
+++ ecom/src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,19 @@
+import {
+  SignInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await SignInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+
+  return (
+    <div>
+      <h1>Sign IN Page</h1>
+      <button onClick={logGoogleUser}>sign in with Google Popup</button>
+    </div>
+  );
+};
+export default SignIn;
---
+++ ecom/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,59 @@
+import { initializeApp } from "firebase/app";
+
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyASTZTW0HpgBcKePU4RilXSy2Wd2iGhe3g",
+  authDomain: "ecom-8d700.firebaseapp.com",
+  projectId: "ecom-8d700",
+  storageBucket: "ecom-8d700.appspot.com",
+  messagingSenderId: "346003646188",
+  appId: "1:346003646188:web:276b25db74b5984acce7c9",
+  measurementId: "G-QFLDB0YNMB",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const SignInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("Error creating user", error.message);
+    }
+  }
+  return userDocRef;
+};
